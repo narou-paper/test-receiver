9a35236172429847d4688c91a85ca30b9039a1af
yui <ynakano1127@gmail.com>
2020-11-22 02:41:35 +0900

    [add]テンプレートを変更

--- index.js
+++ index.js
@@ -20,7 +20,32 @@ app.post('/', (req, res) => {
             height: 880,
             deviceScaleFactor: 1,
         })
-        await page.goto(`file://${__dirname}/template.html`)
+        await page.goto(`file://${__dirname}/template/template.html`)
+
+
+        await page.evaluate(() => {
+            //ここらへんリクエストとすり合わせてください
+            const novelContainer = document.getElementById('novel')
+            //タイトルのクラスをつければ真ん中になる
+            const pElement = document.createElement('p')
+            pElement.className = 'novel_subtitle'
+            pElement.textContent = 'たいとるだよん'
+            novelContainer.appendChild(pElement)
+            //作者とかを下に持ってきたい場合はtext-alignのクラスをcssファイルに追加して
+
+            //ただの文章は追加するだけ
+            for(let i=0;i<30;i++){
+                let text = 'キリトかなー'
+                for(let j=0;j<i;j++)
+                    text += 'w'
+                text += 'やっぱwww。'
+
+                const pElement = document.createElement('p')
+                pElement.textContent = text
+                novelContainer.appendChild(pElement)
+            }
+        })
+        await page.waitForTimeout(500) //要検討.ファイルのIOが遅いとかで500ms以上ほしいときに対応できなくなってしまうので
         await page.pdf({path: 'episode.pdf', width: 528, height: 880});
 
         await browser.close()
